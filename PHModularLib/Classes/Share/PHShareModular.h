d4f8f290c88400a6e6abd1a66f4b2cf5be865c3f
Alex <Alex@Alex-2.local>
2017-09-07 15:17:16 +0800

    no message

---
+++ PHModularLib/Classes/Share/PHShareModular.h
@@ -0,0 +1,13 @@
+//
+//  PHShareModular.h
+//  Pods
+//
+//  Created by Alex on 2017/9/6.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHShareModular : NSObject
+
+@end
---
+++ PHModularLib/Classes/Share/PHShareModular.m
@@ -0,0 +1,18 @@
+//
+//  PHShareModular.m
+//  Pods
+//
+//  Created by Alex on 2017/9/6.
+//
+//
+
+#import "PHShareModular.h"
+#import <ShareSDK/ShareSDK.h>
+
+@implementation PHShareModular
+    
++ (void)load {
+    
+}
+
+@end
